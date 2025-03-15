4a86478631375ce329d4116b44b825c2f12e5248
Eng Chong Yock <engchongyock@Engs-Laptop.local>
2025-03-16 04:12:08 +0800

    Amended sys path

--- data/process_data.py
+++ data/process_data.py
@@ -3,8 +3,7 @@ import pandas as pd
 import sys
 from datetime import datetime
 
-parent_dir = os.path.abspath(os.path.join(os.getcwd(), ''))
-sys.path.append(parent_dir)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from config import get_config
 
